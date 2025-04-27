861dd0837a939a1d4c67859994889b85715da2f0
vyu <clearvyu@naver.com>
2025-04-27 19:25:11 +0900

    feat: apply get/edit/delete a account api on sheet-provider

---
+++ features/accounts/components/edit-account-sheet.tsx
@@ -0,0 +1,91 @@
+import { z } from 'zod';
+
+import { useOpenAccount } from '@/features/accounts/hooks/use-open-account';
+import { AccountForm } from '@/features/accounts/components/account-form';
+import { useEditAccount } from '@/features/accounts/api/use-edit-account';
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account';
+
+import { useConfirm } from '@/hooks/use-confirm';
+import { insertAccountSchema } from '@/db/schema';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+import { useGetAccount } from '@/features/accounts/api/use-get-account';
+import { Loader2 } from 'lucide-react';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const EditAccountSheet = () => {
+  const { isOpen, onClose, id } = useOpenAccount();
+
+  const [ConformDialog, confirm] = useConfirm(
+    'Are you sure?',
+    'You are about to delete this transaction.'
+  );
+
+  const accountQuery = useGetAccount(id);
+  const editMutation = useEditAccount(id);
+  const deleteMutation = useDeleteAccount(id);
+
+  const isPending = editMutation.isPending || deleteMutation.isPending;
+  const isLoading = accountQuery.isLoading;
+
+  const onSubmit = (values: FormValues) => {
+    editMutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+    console.log('Form submitted:', values);
+  };
+
+  const onDelete = async () => {
+    const ok = await confirm();
+    if (ok) {
+      deleteMutation.mutate(undefined, { onSuccess: () => onClose() });
+    }
+  };
+
+  const defaultValues = accountQuery.data
+    ? {
+        name: accountQuery.data.name,
+      }
+    : { name: '' };
+
+  return (
+    <>
+      <ConformDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        {/* <SheetContent className="space-y-4"> */}
+        <SheetContent className="">
+          <SheetHeader>
+            <SheetTitle>Edit Account</SheetTitle>
+            <SheetDescription>Edit an existing account</SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader2 className="size-4 text-muted-foreground animate-spin" />
+            </div>
+          ) : (
+            <AccountForm
+              id={id}
+              onSubmit={onSubmit}
+              onDelete={onDelete}
+              disabled={isPending}
+              defaultValues={defaultValues}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
